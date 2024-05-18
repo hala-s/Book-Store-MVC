befc45ab7e54781d58c68a174c7d6bbdd9120428
hala-s <hala_mallak6522@hotmail.com>
2024-05-18 16:30:34 +0300

    initial

---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet <Category> categories { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
